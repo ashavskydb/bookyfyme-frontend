2f279059451e176edd94d76e08abe61d5d9c09ca
Dmitry Ashavskiy <160394751+ashavskydb@users.noreply.github.com>
2024-07-29 12:09:43 +0300

    Update eventService.jsx

--- src/services/eventService.jsx
+++ src/services/eventService.jsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/bandsintown';
 
-const searchEvents = async (city, date, region) => {
-  const response = await axios.post(`${API_URL}/`, { city, date, region });
+const searchEvents = async (city, date) => {
+  const response = await axios.post(`${API_URL}/`, { city, date });
   return response.data;
 };
 
