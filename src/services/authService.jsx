9eabedd45b6fa85b61e84118a496a34aa5eaf106
Dmitry Ashavskiy <160394751+ashavskydb@users.noreply.github.com>
2024-08-18 13:57:00 +0300

    Update authService.jsx

--- src/services/authService.jsx
+++ src/services/authService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/auth';
+const API_URL = import.meta.env.VITE_API_URL || 'https://bookyfyme-backend.onrender.com/api/auth';
 
 const register = async (email, password) => {
   await axios.post(`${API_URL}/register`, { email, password });
