946377c05f9922f0cb3984a060e1b8d47f19f725
Dmitry Ashavskiy <160394751+ashavskydb@users.noreply.github.com>
2024-08-18 13:57:20 +0300

    Update calendarService.jsx

--- src/services/calendarService.jsx
+++ src/services/calendarService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/calendars';
+const API_URL = 'https://bookyfyme-backend.onrender.com/api/calendars';
 
 const getEvents = async () => {
   const response = await axios.get(`${API_URL}`);
