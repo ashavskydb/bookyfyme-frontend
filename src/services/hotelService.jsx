c7518e478e750230f4f00531e9f394c402bc8760
Dmitry Ashavskiy <160394751+ashavskydb@users.noreply.github.com>
2024-08-21 10:48:00 +0300

    Update hotelService.jsx

--- src/services/hotelService.jsx
+++ src/services/hotelService.jsx
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL || 'https://bookyfyme-backend.onrender.com/api/accommodations';
 
-const searchAccommodations = async (city, checkInDate, checkOutDate) => {
+const search_Accommodations = async (city, checkInDate, checkOutDate) => {
   const response = await axios.post(`${API_URL}/search`, { city, checkInDate, checkOutDate });
     return response.data;
 };
 
 const hotelService = {
-  searchAccommodations
+  search_Accommodations
 };
 
 export default hotelService;
